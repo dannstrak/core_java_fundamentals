2f3072eaba27297c8ca40594c75f484ce355b3bf
Alejandro <d.annstrak@gmail.com>
2026-08-17 17:35:08 -0500

    Ejercicio

---
+++ src/Capitulo_09_Collections/Exercise_01_LInkedList/HistorialNavegador.java
@@ -0,0 +1,30 @@
+package Capitulo_09_Collections.Exercise_01_LInkedList;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class HistorialNavegador {
+
+    static void main() {
+        List<String> historial = new LinkedList<>();
+        historial.add("Google");
+        historial.add("StackOverflow");
+        historial.add("GitHub");
+        // Insertamos elementos en la primera posicion de la lista.
+        historial.addFirst( "NuevaPestana");
+        historial.addLast("Cierre de Sesion");
+        // Creamos una listIterator.
+        ListIterator<String> iterador = historial.listIterator();
+        while(iterador.hasNext()){
+            var elemento = iterador.next();
+            System.out.print(elemento + "->");
+        }
+        System.out.println(" ");
+        while(iterador.hasPrevious()){
+            var elemento = iterador.previous();
+            System.out.print(elemento + "<-");
+        }
+    }
+}

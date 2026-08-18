7a0a1f464b2f54c3f1f6b231b66897acd6f89f03
Alejandro <d.annstrak@gmail.com>
2026-08-18 16:36:45 -0500

    Ejercicio

--- src/Capitulo_09_Collections/Exercise_03_HashSet/AnalisisLogsServidor.java
+++ src/Capitulo_09_Collections/Exercise_03_HashSet/AnalisisLogsServidor.java
@@ -3,6 +3,7 @@ package Capitulo_09_Collections.Exercise_03_HashSet;
 import javax.swing.text.html.HTMLDocument;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class AnalisisLogsServidor {
     static void main(String[] args) {
@@ -27,6 +28,24 @@ public class AnalisisLogsServidor {
         for(String elemento : usuarioDia2){
             System.out.print(elemento + " ");
         }
+
+        // Uso de contains method.
+        var esSospechosoD1 = usuarioDia1.contains("ID-500");
+        System.out.println("¿El usuario ID-500 entró el Día 1? " + esSospechosoD1);
+
+        Set<String> usuariosTotales = new HashSet<>(usuarioDia1);
+        usuariosTotales.addAll(usuarioDia2);
+        System.out.println("Unión (Todos los únicos): " + usuariosTotales);
+
+        Set<String> usuariosFieles = new HashSet<>(usuarioDia1);
+        usuariosFieles.retainAll(usuarioDia2);
+        System.out.println("Intersección (Entraron ambos días): " + usuariosFieles);
+
+        Set<String> usuariosFugados = new HashSet<>(usuarioDia1);
+        usuariosFugados.removeAll(usuarioDia2);
+        System.out.println("Diferencia (Entraron Día 1, pero no Día 2): " + usuariosFugados);
+
+
 
     }
 }

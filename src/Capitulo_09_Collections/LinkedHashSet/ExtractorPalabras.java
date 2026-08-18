da944527e6efe773ef61ff333744cd2e498fb000
Alejandro <d.annstrak@gmail.com>
2026-08-18 17:20:48 -0500

    Ejercicio

---
+++ src/Capitulo_09_Collections/LinkedHashSet/ExtractorPalabras.java
@@ -0,0 +1,23 @@
+package Capitulo_09_Collections.LinkedHashSet;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ExtractorPalabras {
+
+    static void main() {
+        Set<String> vocabulario = new LinkedHashSet<>();
+        vocabulario.add("matriz");
+        vocabulario.add("Vector");
+        vocabulario.add("tensor");
+        vocabulario.add("matriz");
+        vocabulario.add("Escala");
+
+        for (String palabra : vocabulario){
+            System.out.println(palabra);
+        }
+
+
+    }
+}

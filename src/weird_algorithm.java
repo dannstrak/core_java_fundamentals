3149a6e319345d1a600debd0ad4ce9c24be8e064
Alejandro <d.annstrak@gmail.com>
2026-08-22 10:49:06 -0500

    weird_algorithm

--- src/weird_algorithm.java
+++ src/weird_algorithm.java
@@ -3,22 +3,22 @@ import java.util.ArrayList;
 public class weird_algorithm {
     static ArrayList<Long> arregloFinal = new ArrayList<>();
     public static Long weirdAlgorithm(long n){
+        arregloFinal.add(n);
         if (n==1){
-            arregloFinal.add(1L);
             return 1L;
         }else{
             if(n%2 == 0){
-                Long valor = (long) (n/2);
-                arregloFinal.add(valor);
-                return weirdAlgorithm(valor);
+                return weirdAlgorithm(n/2);
             }else{
-                Integer valor = Math.toIntExact((n * 3) + 1);
-                arregloFinal.add(valor);
-                return weirdAlgorithm(valor);
+                return weirdAlgorithm((n*3)+1);
             }
         }
-        for(Integer valor : arregloFinal){
-            System.out.println(valor);
+    }
+
+    static void main(String[] args) {
+        weirdAlgorithm(3);
+        for(long e : arregloFinal){
+            System.out.print(e + " ");
         }
     }
 }

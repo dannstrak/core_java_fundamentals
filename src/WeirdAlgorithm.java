e0ad18978885a66c954a2464b391beebc5dc8425
Alejandro <d.annstrak@gmail.com>
2026-08-22 11:02:39 -0500

    Ejercicio

--- src/WeirdAlgorithm.java
+++ src/WeirdAlgorithm.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class weird_algorithm {
+public class WeirdAlgorithm {
     static ArrayList<Long> arregloFinal = new ArrayList<>();
     public static Long weirdAlgorithm(long n){
         arregloFinal.add(n);
@@ -16,7 +16,7 @@ public class weird_algorithm {
         }
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         if (sc.hasNextLong()){
             long n = sc.nextLong();

0b44457b890fa27a18e29fce7652e6844f15f901
Alejandro <d.annstrak@gmail.com>
2026-08-23 21:16:03 -0500

    Check Permutation

---
+++ src/Cracking_the_Coding/Chapter_01/CheckPermutation.java
@@ -0,0 +1,34 @@
+package Cracking_the_Coding.Chapter_01;
+
+import java.util.ArrayDeque;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class CheckPermutation {
+    public static boolean checkPermutation (String cadenaOriginal, String posiblePermutacion){
+        int[] arregloFinal = new int[128];
+        if (cadenaOriginal.length() != posiblePermutacion.length())
+            return false;
+        for (int i = 0; i < cadenaOriginal.length(); i++){
+            int value = cadenaOriginal.charAt(i);
+            arregloFinal[value] ++;
+        }
+        for (int i = 0; i < posiblePermutacion.length(); i++){
+            int value = cadenaOriginal.charAt(i);
+            if (arregloFinal[value] < 0) {
+                return false;
+            }else{
+                arregloFinal[value] --;
+            }
+        }
+        return  true;
+    }
+
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena = sc.nextLine();
+        String posiblePermutacion = sc.nextLine();
+        boolean resultado = checkPermutation(cadena, posiblePermutacion);
+        System.out.println("El resultado es el siguiente: "+resultado);
+    }
+}

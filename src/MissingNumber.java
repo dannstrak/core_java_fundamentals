9261072f78d1239d482609bc31df6f3a17be9c0e
Alejandro <d.annstrak@gmail.com>
2026-08-25 16:23:11 -0500

    Missing Number

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -2,28 +2,29 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class MissingNumber {
-
-    public static int valorTotal (int maxValue){
-        return (maxValue * (maxValue +1))/2;
-    }
-    public static int missing(int maxValue, HashSet<Integer> numerosDados){
-        int valueMaxSet = maxValue - 1;
-        HashSet<Integer> numeros = new HashSet<>(numerosDados);
+    public static int misingNumber (int limiteMaximo, int[] enteros){
+        int valueFinal = (limiteMaximo*(limiteMaximo+1))/2;
         int contador = 0;
-        for (Integer e : numeros){
-            contador += e;
+        if (limiteMaximo == 1){
+            return 1;
+        }else{
+            for(int i = 0; i<enteros.length; i++){
+                contador += enteros[i];
+            }
         }
-        return valorTotal(maxValue) - contador;
+        return valueFinal-contador;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int valor = sc.nextInt();
-        HashSet<Integer> valores = new HashSet<>(valor -1);
-        for (int i = 0; i < valor - 1; i++) {
-            valores.add(sc.nextInt());
+        int limite = sc.nextInt();
+        int arreglo[] = new int[limite-1];
+        if (sc.hasNextInt()){
+            for (int i = 0; i<arreglo.length; i++){
+                arreglo[i] = sc.nextInt();
+            }
         }
-        int valorFinal = missing(valor, valores);
+        int valorFinal = misingNumber(limite, arreglo);
         System.out.println(valorFinal);
     }
 }

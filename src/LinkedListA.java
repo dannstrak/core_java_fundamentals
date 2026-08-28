b16b0794829d299b1dffd02a91f0efe3a67ea013
Alejandro <d.annstrak@gmail.com>
2026-08-27 21:03:21 -0500

    LinkedList

---
+++ src/LinkedListA.java
@@ -0,0 +1,42 @@
+class Link{
+    int data;
+    int identificador;
+    Link next;
+    public Link(int data, int identificador){
+        this.data = data;
+        this.identificador = identificador;
+    }
+    public void displayLink(){
+        System.out.print("{" + identificador + ", " + data + "} ");
+    }
+}
+public class LinkedListA {
+    private Link first;
+
+    public LinkedListA(Link first){
+        first = null;
+    }
+    public boolean isEmpty (){
+        return first == null;
+    }
+    // InsertFirst.
+    public void insertFirst (int identificador, int dato){
+        Link newLink = new Link(dato, identificador);
+        newLink.next = first.next;
+        first.next = newLink;
+    }
+    // DeleteFirst.
+    public void deleteFirst (int identificador){
+        Link removeLink = first.next;
+        first.next = removeLink.next;
+    }
+    // DisplayChainofLinks.
+    public void displayLinkedList (){
+        Link current = first;
+        while (!isEmpty()){
+            current.displayLink();
+            current = current.next;
+        }
+    }
+
+}
